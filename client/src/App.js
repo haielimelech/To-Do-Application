56714b0dcb6ab4a9935852530f13897aa392135e
hai <hai842@gmail.com>
2023-02-08 20:30:18 +0200

    bla bla

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import ListHeader from './components/ListHeader'
 const App=() => {
   return (
     <div className = "app">
-    <ListHeader listName ={'Is it Work Both laptop and pc !? yes!'} />
+    <ListHeader listName ={'Holiday :)'} />
     </div>
   )
 }
